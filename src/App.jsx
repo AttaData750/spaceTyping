af6e14d2b56bc86aa8fd6a05f8338598e347d751
jeremy affolter <jeremy.affolter@gmail.com>
2026-04-11 15:17:28 +0100

    first commit at home

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div style={styles.outer}>
-      <div style={styles.inner}>App</div>
+      <div style={styles.inner}>App hhhhhhhhhhhhhh</div>
     </div>
   )
 }
